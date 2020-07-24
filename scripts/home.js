6177b30d732565465f09b81f18c856fbf78044d0
Alpha-30-creator <abdurrahmansaad2020@gmail.com>
2020-07-24 23:52:54 +0500

    update

--- scripts/home.js
+++ scripts/home.js
@@ -7,3 +7,33 @@ function myFunction() {
     x.className = "topnav";
     }
 }
+function Exec_team(){
+  document.querySelector(".direc-team").style.display = "none";
+  document.querySelector(".head-team").style.display = "none";
+  if (document.querySelector(".exec-team").style.display == "none") {
+    document.querySelector(".exec-team").style.display = "flex";
+  }
+  else {
+    document.querySelector(".exec-team").style.display = "none";
+  }
+}
+function Direc_team(){
+  document.querySelector(".exec-team").style.display = "none";
+  document.querySelector(".head-team").style.display = "none";
+  if (document.querySelector(".direc-team").style.display == "none") {
+    document.querySelector(".direc-team").style.display = "flex";
+  }
+  else {
+    document.querySelector(".direc-team").style.display = "none";
+  }
+}
+function Head_team(){
+  document.querySelector(".exec-team").style.display = "none";
+  document.querySelector(".direc-team").style.display = "none";
+  if (document.querySelector(".head-team").style.display == "none") {
+    document.querySelector(".head-team").style.display = "flex";
+  }
+  else {
+    document.querySelector(".head-team").style.display = "none";
+  }
+}
